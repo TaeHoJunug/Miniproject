c20dc49954a1cb474724c33b8c7c9510ee68e41c
HKEDU <HKEDU@DESKTOP-EB89O00>
2023-11-24 16:51:16 +0900

    정태호

--- 19_miniproject/src/main/java/com/hk/calboard/controller/MemberController.java
+++ 19_miniproject/src/main/java/com/hk/calboard/controller/MemberController.java
@@ -86,6 +86,7 @@ public class MemberController {
 	@GetMapping(value = "/login")
 	public String loginForm(Model model) {
 		model.addAttribute("loginCommand", new LoginCommand());
+		System.out.println("로그인폼 이동");
 		return "member/login";
 	}
 	
