a2c9cb548fe79b3de0cd7cc24527dfbb6bfa1f8a
HKEDU <HKEDU@DESKTOP-EB89O00>
2023-11-24 17:18:52 +0900

    ㅁㄴㅇ

--- 19_miniproject/src/main/java/com/hk/calboard/command/UpdateUserCommand.java
+++ 19_miniproject/src/main/java/com/hk/calboard/command/UpdateUserCommand.java
@@ -5,45 +5,113 @@ import org.hibernate.validator.constraints.Length;
 import jakarta.validation.constraints.NotBlank;
 
 public class UpdateUserCommand {
+   
+   @NotBlank(message="아이디를 입력하세요")
+   private String id;
+   @NotBlank(message="이름을 입력하세요")
+   private String name;
+   @NotBlank(message="비밀번호를 입력하세요")
+   @Length(min= 8, max=16,message="8자리이상,16자리 이하로입력하세요")
+   private String password;
+   @NotBlank(message="이메일을 입력하세요")
+   private String email;
+   @NotBlank(message="주소를 입력하세요")
+   private String address;
+    
+   
+   public UpdateUserCommand() {
+      super();
+   }
 
-	@NotBlank(message = "아이디를 입력해주세요")
-	private String id;
-	
-	@NotBlank(message = "패스워드를 입력해주세요")
-	@Length(min = 8 , max = 16, message = "8자리이상, 16자이하로 입력하세요")
-	private String password;
 
-	public UpdateUserCommand() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+   public UpdateUserCommand(@NotBlank(message = "아이디를 입력하세요") String id, @NotBlank(message = "이름을 입력하세요") String name,
+         @NotBlank(message = "비밀번호를 입력하세요") @Length(min = 8, max = 16, message = "8자리이상,16자리 이하로입력하세요") String password,
+         @NotBlank(message = "이메일을 입력하세요") String email, @NotBlank(message = "주소를 입력하세요") String address) {
+      super();
+      this.id = id;
+      this.name = name;
+      this.password = password;
+      this.email = email;
+      this.address = address;
+   }
 
-	public UpdateUserCommand(@NotBlank(message = "아이디를 입력해주세요") String id,
-			@NotBlank(message = "패스워드를 입력해주세요") @Length(min = 8, max = 16, message = "8자리이상, 16자이하로 입력하세요") String password) {
-		super();
-		this.id = id;
-		this.password = password;
-	}
 
-	public String getId() {
-		return id;
-	}
+   @Override
+   public String toString() {
+      return "UpdateUser [id=" + id + ", name=" + name + ", password=" + password + ", email=" + email
+            + ", address=" + address + "]";
+   }
 
-	public void setId(String id) {
-		this.id = id;
-	}
 
-	public String getPassword() {
-		return password;
-	}
+   public String getId() {
+      return id;
+   }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
-	@Override
-	public String toString() {
-		return "UpdateUser [id=" + id + ", password=" + password + "]";
-	}
-	
+   public void setId(String id) {
+      this.id = id;
+   }
+
+
+   public String getName() {
+      return name;
+   }
+
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+
+   public String getPassword() {
+      return password;
+   }
+
+
+   public void setPassword(String password) {
+      this.password = password;
+   }
+
+
+   public String getEmail() {
+      return email;
+   }
+
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+
+   public String getAddress() {
+      return address;
+   }
+
+
+   public void setAddress(String address) {
+      this.address = address;
+   }
+   
+   
+   
+   
+   
+   
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
